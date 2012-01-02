85e1abd59417b4c06722eef6a64ed08c971250af
Mateus <seanlilmateus@yahoo.de>
2012-01-02 14:53:02 +0100

    Update TVFrancaise/LTPlayerView.rb

--- TVFrancaise/LTPlayerView.rb
+++ TVFrancaise/LTPlayerView.rb
@@ -6,39 +6,39 @@
 #  Copyright 2011 Sean Coorp. INC. All rights reserved.
 #
 class LTPlayerView < NSView	
-	FILLMODES = [AVLayerVideoGravityResize, AVLayerVideoGravityResizeAspectFill, AVLayerVideoGravityResizeAspect]
+    FILLMODES = [AVLayerVideoGravityResize, AVLayerVideoGravityResizeAspectFill, AVLayerVideoGravityResizeAspect]
 
-	def self.layerClass
-      AVPlayerLayer.class
+  def self.layerClass
+    AVPlayerLayer.class # doesn't work only on IOS
   end
 	
-	def awakeFromNib
-		self.wantsLayer = true
-		color = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1)
+  def awakeFromNib
+    self.wantsLayer = true
+    color = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1)
     self.layer.backgroundColor = color
     CGColorRelease(color)
-	end
+  end
 
   def initWithURL url
-		asset = AVURLAsset.URLAssetWithURL url, options:nil
-		hander_block = -> do
-			@status = asset.statusOfValueForKey("tracks", error:nil)
-			#The completion block goes here.
-			Dispatch::Queue.main.async do
-				error = nil          
-				if (@status == AVKeyValueStatusLoaded)
-					player_item = AVPlayerItem.playerItemWithAsset(asset)            
-					player = AVPlayer.playerWithPlayerItem player_item
-					self.player = player
-					player.play 
-				else
-					# You should deal with the error appropriately.
-					NSLog("The asset's tracks were not loaded:#{error[0].localizedDescription}")
-				end
-			end
-		end
-		asset.loadValuesAsynchronouslyForKeys ["tracks"], completionHandler:hander_block
-	end
+    asset = AVURLAsset.URLAssetWithURL url, options:nil
+    hander_block = -> do
+      @status = asset.statusOfValueForKey("tracks", error:nil)
+      #The completion block goes here.
+      Dispatch::Queue.main.async do
+        error = nil          
+        if (@status == AVKeyValueStatusLoaded)
+          player_item = AVPlayerItem.playerItemWithAsset(asset)            
+          player = AVPlayer.playerWithPlayerItem player_item
+          self.player = player
+          player.play 
+        else
+          # You should deal with the error appropriately.
+          NSLog("The asset's tracks were not loaded:#{error[0].localizedDescription}")
+        end
+      end
+    end
+    asset.loadValuesAsynchronouslyForKeys ["tracks"], completionHandler:hander_block
+  end
 
     
   def player
@@ -47,23 +47,23 @@ class LTPlayerView < NSView
   
   def start_playingURL(contentURL)
     new_asset = AVURLAsset.URLAssetWithURL contentURL, options:nil
-		new_player_item = AVPlayerItem.playerItemWithAsset new_asset
-		player.pause
-		player.replaceCurrentItemWithPlayerItem new_player_item
-		player.play
+    new_player_item = AVPlayerItem.playerItemWithAsset new_asset
+    player.pause
+    player.replaceCurrentItemWithPlayerItem new_player_item
+    player.play
   end
   
   def player=(player)
-    av_player_layer = AVPlayerLayer.playerLayerWithPlayer player
-		color = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1)
-    av_player_layer.backgroundColor = color
+    avplayer_layer = AVPlayerLayer.playerLayerWithPlayer player
+    color = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1)
+    avplayer_layer.backgroundColor = color
     CGColorRelease(color)
-    av_player_layer.videoGravity = AVLayerVideoGravityResizeAspect
-    self.layer = av_player_layer
+    avplayer_layer.videoGravity = AVLayerVideoGravityResizeAspect
+    self.layer = avplayer_layer
   end
 	
-	def video_fillmode mode
-		self.layer.videoGravity = FILLMODES[mode]
-	end
+  def video_fillmode mode
+    self.layer.videoGravity = FILLMODES[mode]
+  end
 end
 
