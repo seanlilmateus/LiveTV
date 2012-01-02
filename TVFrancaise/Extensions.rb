5cc9940adb4423030c0c17b2b02f7797d5101a33
Mateus <seanlilmateus@yahoo.de>
2012-01-02 14:59:44 +0100

    Update TVFrancaise/Extensions.rb

--- TVFrancaise/Extensions.rb
+++ TVFrancaise/Extensions.rb
@@ -8,65 +8,67 @@
 module Kernel # Localization Extensions
   private
   def NSLocalizedString(key, value:value)
-		value = key if value.nil?
+    value = key if value.nil?
     NSBundle.mainBundle.localizedStringForKey(key, value:value, table:nil)
   end
 end
 
 class NSTimer # Timer Extenstion
-	def self.scheduledTimerWithTimeInterval interval, repeats: repeat_flag, block: block
-		self.scheduledTimerWithTimeInterval interval, 
-																target: self, 
-															selector: 'executeBlockFromTimer:', 
-															userInfo: block, 
-															 repeats: repeat_flag
-	end
-	def self.timerWithTimeInterval interval, repeats: repeat_flag, block: block
-		self.timerWithTimeInterval interval, 
-											 target: self, 
-										 selector: 'executeBlockFromTimer:', 
-										 userInfo: block, 
-											repeats: repeat_flag
-	end
-	def self.executeBlockFromTimer aTimer
-		blck = aTimer.userInfo
-		time = aTimer.timeInterval
-		blck[time] if blck
-	end
+  def self.scheduledTimerWithTimeInterval interval, repeats: repeat_flag, block: block
+    self.scheduledTimerWithTimeInterval interval, 
+				target: self, 
+			      selector: 'executeBlockFromTimer:', 
+			      userInfo: block, 
+			       repeats: repeat_flag
+  end
+  
+  def self.timerWithTimeInterval interval, repeats: repeat_flag, block: block
+    self.timerWithTimeInterval interval, 
+			target: self, 
+		      selector: 'executeBlockFromTimer:', 
+		      userInfo: block, 
+		       repeats: repeat_flag
+  end
+  
+  def self.executeBlockFromTimer aTimer
+    blck = aTimer.userInfo
+    time = aTimer.timeInterval
+    blck[time] if blck
+  end
 end
 
 
 class NSEvent # NSEvent Extensions
-	def self.cmdKey
-		(GetCurrentKeyModifiers() & cmdKey) != 0
-	end
+  def self.cmdKey
+    (GetCurrentKeyModifiers() & cmdKey) != 0
+  end
 
-	def self.shiftKey
-		(GetCurrentKeyModifiers() & (shiftKey | rightShiftKey)) != 0
-	end
+  def self.shiftKey
+    (GetCurrentKeyModifiers() & (shiftKey | rightShiftKey)) != 0
+  end
 
-	def self.optionKey
-		(GetCurrentKeyModifiers() & (optionKey | rightOptionKey)) != 0
-	end
+  def self.optionKey
+    (GetCurrentKeyModifiers() & (optionKey | rightOptionKey)) != 0
+  end
 
-	def self.controlKey
-		(GetCurrentKeyModifiers() & (controlKey | rightControlKey)) != 0
-	end
+  def self.controlKey
+    (GetCurrentKeyModifiers() & (controlKey | rightControlKey)) != 0
+  end
 
-	def cmdKey
-		self.modifierFlags & NSCommandKeyMask != 0
-	end
+  def cmdKey
+    self.modifierFlags & NSCommandKeyMask != 0
+  end
 
-	def shiftKey
-		self.modifierFlags & NSShiftKeyMask != 0
-	end
+  def shiftKey
+    self.modifierFlags & NSShiftKeyMask != 0
+  end
 
-	def optionKey
-		self.modifierFlags & NSAlternateKeyMask != 0
-	end
+  def optionKey
+    self.modifierFlags & NSAlternateKeyMask != 0
+  end
 
-	def controlKey
-		self.modifierFlags & NSControlKeyMask != 0
-	end
+  def controlKey
+    self.modifierFlags & NSControlKeyMask != 0
+  end
 end
 
